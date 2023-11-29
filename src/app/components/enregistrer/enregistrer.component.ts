6544ca250732173baa6bf998492d4469fed38485
Meranic <meranic77@gmail.com>
2023-11-29 06:42:24 +0100

    commit final

--- src/app/components/enregistrer/enregistrer.component.ts
+++ src/app/components/enregistrer/enregistrer.component.ts
@@ -1,15 +1,18 @@
+// Importation des modules nécessaires depuis Angular
 import { Component } from '@angular/core';
 import { User } from '../../../models/user';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 
+// Définition du composant Angular
 @Component({
   selector: 'app-enregistrer',
   templateUrl: './enregistrer.component.html',
   styleUrls: ['./enregistrer.component.css'],
 })
 export class EnregistrerComponent {
+  // Déclaration et initialisation d'un objet utilisateur
   user: User = {
     username: '',
     email: '',
@@ -19,25 +22,31 @@ export class EnregistrerComponent {
     to_likes: [],
   };
 
+  // Variables pour gérer les erreurs de confirmation de mot de passe et l'état de l'inscription
   confirmMdpError = false;
   inscriptionOK = true;
   isFormSubmit = false;
 
+  // Constructeur du composant avec injection de dépendances
   constructor(private userService: UserService, private router: Router) {}
 
+  // Méthode appelée lors de la soumission du formulaire d'inscription
   inscrire(inscriptionForm: NgForm) {
     this.isFormSubmit = true;
 
-    // Verifier si les mots de passe correspondent
+    // Vérification si les mots de passe correspondent
     this.confirmMdpError = this.user.password !== this.user.password_validate;
 
+    // Vérification de la validité du formulaire et de l'absence d'erreur de confirmation de mot de passe
     if (inscriptionForm.valid && !this.confirmMdpError) {
-      // Si tous les champs sont valides, alors continuez avec l'inscription.
+      // Si tous les champs sont valides, envoi de la demande d'inscription au service utilisateur
       this.userService.inscriptionUtilisateur(this.user).subscribe({
         next: (response) => {
+          // Redirection vers la page de connexion après une inscription réussie
           this.router.navigate(['../Connexion']);
         },
         error: (error) => {
+          // Gestion des erreurs lors de l'inscription
           this.inscriptionOK = false;
         },
       });
